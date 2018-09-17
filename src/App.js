760dd2ed9d4e05a8555128bcd37b5dfb9c4fdc33
Joe Anderson <joseph.anderson423@gmail.com>
2018-09-17 16:10:36 -0600

    replaced ternary operator with alternative for conditional code

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
     const doesShow = this.state.showPersons;
     this.setState({showPersons: !doesShow});
   }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -46,29 +47,36 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div> 
+
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-
-        { this.state.showPersons === true ?
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind( this, 'Max!')}
-              changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
-          </div> : null
-        } 
+        {persons}
       </div>
     );
   }
